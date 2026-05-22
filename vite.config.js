b360a4be4ce13bec0cb97512f1eceec4e76eaf80
syf-atelier <syf.atelier@gmail.com>
2026-05-22 17:11:04 -0400

    Ajuste de ruta base para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/mis-finanzas/', // <-- LA RUTA DE TU REPOSITORIO
+  base: '/Mis-finanzas/', // <-- LA RUTA DE TU REPOSITORIO
 })
